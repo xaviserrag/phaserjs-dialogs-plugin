a4c6fd7bd66a18d0b5c4c4024fb2c579a5c351e8
Xavi Serra <xaviserrag@gmail.com>
2015-02-27 11:50:45 +0100

    (Feat) Added bg to the dialog

--- dist/js/dialogs.js
+++ dist/js/dialogs.js
@@ -4,17 +4,19 @@
  * Dialogs plugin.
  */
 Phaser.Plugin.Dialogs = function (game) {
-    Phaser.Plugin.call(this, game);
+  Phaser.Plugin.call(this, game);
+  this.game = game;
 
   this.dialogs = game.add.group();
-    this.dialogs.inputEnabled = true;
+  this.dialogs.name = 'Dialogs';
 };
 
 Phaser.Plugin.Dialogs.prototype = Object.create(Phaser.Plugin.prototype);
 Phaser.Plugin.Dialogs.prototype.constructor = Phaser.Plugin.Dialogs;
 
 Phaser.Plugin.Dialogs.prototype.createDialog = function (config) {
-  var type = config.type || '',
+  var self = this,
+      type = config.type || '',
       hasBg = config.hasBg || false,
       hasCloseBtn = config.hasCloseBtn || false,
       bgColor = config.bgColor || "0x000000",
@@ -23,10 +25,17 @@ Phaser.Plugin.Dialogs.prototype.createDialog = function (config) {
       centerH = config.centerH || true,
       objects = config.objects || [];
 
-  var dialog;
+  var dialog = this.game.add.group();
 
   var initBg = function initBg() {
-    console.log("init Bg");
+    var bg;
+
+    bg = self.game.add.graphics(0, 0);
+    bg.beginFill(bgColor, bgAlpha);
+    bg.drawRect(0, 0, self.game.width, self.game.height);
+    bg.inputEnabled = true;
+
+    dialog.add(bg)
   };
 
   hasBg && initBg();
